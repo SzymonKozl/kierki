92cb08ee4d1fdc91ebb56dfe14c612247215c55a
SzymonKozl <szymon.kozlowski.2003@gmail.com>
2024-06-02 13:00:26 +0200

    fixed miliseconds displaying

--- message.cpp
+++ message.cpp
@@ -12,22 +12,21 @@
 #include "ctime"
 #include "sstream"
 #include "iomanip"
+#include "chrono"
 
 Message::Message(net_address  sender, net_address  receiver, std::string payload):
-        registered(std::time(nullptr)),
+        registered(std::chrono::system_clock::now()),
         sender(std::move(sender)),
         receiver(std::move(receiver)),
         payload(std::move(payload))
 {}
 
 std::string Message::toString() {
-    std::string res;
-    std::time_t tt = registered;
     std::stringstream output("");
     output << "[" << formatAddr(sender) << "," << formatAddr(receiver) << ",";
-    std::chrono::system_clock::time_point tp = std::chrono::system_clock::from_time_t(tt);
-    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()) % 1000;
-    std::tm* tm_ptr = std::localtime(&tt);
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(registered.time_since_epoch()) % 1000;
+    time_t tmp = std::chrono::system_clock::to_time_t(registered);
+    std::tm* tm_ptr = std::localtime(&tmp);
     output << std::put_time(tm_ptr, "%Y-%m-%d %H:%M:%S");
     output << '.' << std::setfill('0') << std::setw(3) << milliseconds.count();
     output << "] " << payload << "\n";
